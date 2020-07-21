59cb77716263962d5015eaf51f9a208be0cb5bc1
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-07-21 22:19:23 +0300

    Homework #2

---
+++ hw_2/lesson_2.py
@@ -0,0 +1,75 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import json
+
+DOMAIN = 'https://geekbrains.ru'
+URL = 'https://geekbrains.ru/posts'
+
+
+class GbBlogParse:
+
+    def __init__(self):
+        self.posts_urls = set()
+        self.pagination_urls = set()
+        self.__done_urls = set()
+        self.__post_data = list()
+
+    def get_page_soup(self, url):
+        response = requests.get(url)
+        soup = bs(response.text, 'lxml')
+        return soup
+
+    def run(self, url=None):
+        url = url or URL
+        soup = self.get_page_soup(url)
+        self.pagination_urls.update(self.get_pagination(soup))
+        spam = self.get_posts_urls(soup)
+        self.posts_urls.update(spam)
+        self.get_post_data(spam)
+
+        for url in tuple(self.pagination_urls):
+            if url not in self.__done_urls:
+                self.__done_urls.add(url)
+                self.run(url)
+
+    def get_pagination(self, soup):
+        ul = soup.find('ul', attrs={'class': 'gb__pagination'})
+        a_list = [f'{DOMAIN}{a.get("href")}'
+                  for a in ul.find_all('a') if a.get("href")]
+        return a_list
+
+    def get_posts_urls(self, soup):
+        posts_wrap = soup.find('div', attrs={'class': 'post-items-wrapper'})
+        a_list = [f'{DOMAIN}{a.get("href")}'
+                  for a in posts_wrap.find_all('a',
+                                               attrs={
+                                                   'class': 'post-item__title'
+                                               }
+                                               )
+                  ]
+        return a_list
+
+    def get_post_data(self, urls):
+        for url in urls:
+            data = {}
+            eggs = self.get_page_soup(url)
+            data['title'] = eggs.head.title.string
+            data['post_url'] = url
+            author = eggs.find(itemprop='author')
+            data['writer_name'] = author.string
+            data['writer_url'] = f'{DOMAIN}{author.parent.attrs["href"]}'
+            tags = eggs.find_all(attrs={'class': 'small'})
+            data['tags_urls'] = [f'{DOMAIN}{tag.get("href")}' for tag in tags
+                                 if tag.get("href")]
+            self.__post_data.append(data)
+            print(self.__post_data)
+
+    def save_to_file(self):
+        with open('result.json', 'w', encoding='UTF-8') as file_w:
+            json.dump(self.__post_data, file_w, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    parser = GbBlogParse()
+    parser.run()
+    parser.save_to_file()
