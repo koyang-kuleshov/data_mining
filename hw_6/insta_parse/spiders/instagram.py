3d025197a6562381478aaeb4ef4048b0c659744f
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-08-29 00:32:09 +0300

    Add post_pub_date & like_count

--- hw_6/insta_parse/spiders/instagram.py
+++ hw_6/insta_parse/spiders/instagram.py
@@ -8,17 +8,23 @@
 
 user_name - имя пользвоателя
 user_id - айдишник пользователя
+data = data.user.edge_owner_to_timeline_media.edges[9].node
+if edge_sidecar_to_children in data:
+    data.edge_sidecar_to_children.edges[0].display_url
+else:
+    data.display_url
 post_photos - все фото из поста
-post_pub_date - дата публикации поста
-like_count - количество лайков поста
+post_pub_date - дата публикации поста -- data.taken_at_timestamp
+like_count - количество лайков поста -- data.edge_media_preview_like.count
 Внимание, фото надо скачивать и сохранять, в бд надо хранить как ссылку на фото
 так и путь куда сохранено фото
 """
+import json
+import re
+from copy import deepcopy
+
 import scrapy
 from scrapy.loader import ItemLoader
-import re
-import json
-
 
 from insta_parse.items import InstaParseItem
 
@@ -28,6 +34,10 @@ class InstagramSpider(scrapy.Spider):
     allowed_domains = ['www.instagram.com']
     start_urls = ['https://www.instagram.com/']
     __login_url = 'https://www.instagram.com/accounts/login/ajax/'
+    __api_url = '/graphql/query/'
+    __api_query = 'bfa387b2992c3a52dcbe447467b4b771'
+    variables = {"id": None, "first": 12}
+    users = {}
 
     def __init__(self, login: str, passwd: str, parse_users: list,
                  *args, **kwargs):
@@ -56,8 +66,8 @@ class InstagramSpider(scrapy.Spider):
         if data['authenticated']:
             for user_name in self.parse_users:
                 yield response.follow(f'/{user_name}',
-                                      callback=self.get_user_data,
-                                      cb_kwargs={'user_name': user_name}
+                                      callback=self.get_user_feed,
+                                      cb_kwargs={'user': user_name}
                                       )
 
     def get_user_data(self, response, user_name):
@@ -68,6 +78,47 @@ class InstagramSpider(scrapy.Spider):
             user_name
             )
         )
+        item.add_value(self.get_user_feed(response, user_name))
+        # self.get_user_feed(response, user_name)
+        return item.load_item()
+
+    def get_user_feed(self, response, user):
+        user_id = self.fetch_user_id(response.text, user)
+        user_vars = deepcopy(self.variables)
+        user_vars.update({'id': user_id})
+        yield response.follow(self.make_graphql_url(user_vars),
+                              callback=self.parse_user_posts,
+                              cb_kwargs={'user_vars': user_vars,
+                                         'user': user}
+                              )
+
+    def parse_user_posts(self, response, user_vars, user):
+        item = ItemLoader(InstaParseItem(), response)
+        # post_pub_date - дата публикации поста -- data.taken_at_timestamp
+        # like_count -
+        # количество лайков поста -- data.edge_media_preview_like.count
+        user_id = self.fetch_user_id(
+                                 response.text,
+                                 user
+                                 )
+
+        item.add_value('user_name', user)
+        item.add_value('user_id', self.fetch_user_id(
+            response.text,
+            user
+            )
+        )
+        data = json.loads(response.body)
+        json_posts = (data.get('data').get('user').
+                      get('edge_owner_to_timeline_media').get('edges')
+                      )
+        for post in json_posts:
+            item.add_value('post_pub_date', post.get('node').
+                           get('taken_at_timestamp'))
+            item.add_value('like_count', post.get('node').
+                           get('edge_media_preview_like').
+                           get('count')
+                           )
         return item.load_item()
 
     def fetch_csrf_token(self, text):
@@ -77,10 +128,16 @@ class InstagramSpider(scrapy.Spider):
 
     def fetch_user_id(self, text, username):
         """Используя регулярные выражения парсит переданную строку на наличие
-        `id` нужного пользователя и возвращет его."""
+        'id' нужного пользователя и возвращет его."""
         pattern = r'{\"id\":\"\d+\",\"username\":\"' + username + '\"}'
         matched = re.search(
             pattern,
             text
         ).group()
         return json.loads(matched).get('id')
+
+    def make_graphql_url(self, user_vars):
+        """Возвращает `url` для `graphql` запроса"""
+        result = (f'{self.__api_url}?query_hash={self.__api_query}'
+                  f'&variables={json.dumps(user_vars)}')
+        return result
